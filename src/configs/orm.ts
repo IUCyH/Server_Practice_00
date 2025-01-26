b5f3b8ea508bf3600c2f0a53e16f52a8083490ac
Jaemin <ghdckdwn8456@gmail.com>
2025-01-26 19:54:37 +0900

    [fix] 엔티티 찾지 못하는 문제 수정

--- src/configs/orm.ts
+++ src/configs/orm.ts
@@ -15,7 +15,7 @@ export const AppDataSource = new DataSource({
         timezone: "Asia/Seoul",
         dateStrings: true
     },
-    entities: [__dirname + "/entities/**/*.{js,ts}"],
+    entities: [__dirname + "/entities/**/*.js"],
     subscribers: [],
     migrations: []
 });
