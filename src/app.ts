4b08b28bca306c782fcfde8a8cd856f4eef1a826
Jaemin <ghdckdwn8456@gmail.com>
2025-01-26 19:48:57 +0900

    [fix] 서버 호스트 수정

--- src/app.ts
+++ src/app.ts
@@ -7,7 +7,7 @@ const app = express();
 app.use("/users", userRouter);
 
 const port = 8080;
-const host = "127.0.0.1";
+const host = "0.0.0.0";
 app.listen(port, host, () => {
     console.log(`Server listening at http://${host}:${port}`);
 });
