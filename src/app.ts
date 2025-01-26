0e9dd8cbdf6320d769dc7f4c4b7c5b4f17f522b1
Jaemin <ghdckdwn8456@gmail.com>
2025-01-26 20:22:56 +0900

    [fix] 엔티티 찾지 못하는 문제 수정

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,7 @@
 import "reflect-metadata";
 import express from "express";
 import userRouter from "./features/user/router";
+import { AppDataSource } from "./configs/orm";
 
 const app = express();
 
@@ -8,6 +9,13 @@ app.use("/users", userRouter);
 
 const port = 8080;
 const host = "0.0.0.0";
-app.listen(port, host, () => {
+app.listen(port, host, async () => {
     console.log(`Server listening at http://${host}:${port}`);
+
+    try {
+        await AppDataSource.initialize();
+    } catch(error) {
+        console.log(error);
+        process.exit(1);
+    }
 });
