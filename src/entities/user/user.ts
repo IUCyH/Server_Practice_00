3c2bcaba9e19a59359579fae44f57bbd3a08e112
Jaemin <ghdckdwn8456@gmail.com>
2025-01-26 19:23:25 +0900

    [feat] 엔티티 모델에 데코레이터 추가

--- src/entities/user/user.ts
+++ src/entities/user/user.ts
@@ -1,14 +1,23 @@
 import {
     Entity,
+    PrimaryGeneratedColumn,
+    Column,
     OneToMany
 } from "typeorm";
 import { UserLink } from "./userLink";
 
 @Entity()
 export class User {
+    @PrimaryGeneratedColumn()
     id: number = 0;
+
+    @Column("varchar", { length: 32 })
     uid: string = "";
+
+    @Column("varchar", { length: 10 })
     name: string = "";
+
+    @Column("int")
     level: number = 0;
 
     @OneToMany(() => UserLink, link => link.user)
--- src/entities/user/userLink.ts
+++ src/entities/user/userLink.ts
@@ -1,12 +1,16 @@
 import {
     Entity,
+    PrimaryColumn,
     ManyToOne
 } from "typeorm";
 import { User } from "./user";
 
 @Entity()
 export class UserLink {
+    @PrimaryColumn("int")
     userId: number = 0;
+
+    @PrimaryColumn("varchar", { length: 128 })
     link: string = "";
 
     @ManyToOne(() => User, user => user.links, {
